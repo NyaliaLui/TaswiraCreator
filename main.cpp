1f84d93a0dc0c907629a98949ef21ff8805fd862
Nyalia Lui <luinyalia@gmail.com>
2021-01-23 02:42:52 -0500

    updated main with new example code

--- main.cpp
+++ main.cpp
@@ -2,33 +2,11 @@
 #include "taswira/BitmapImage.h"
 
 int main(void) {
-
     taswira::BitmapImage image(14, 40, 512, 512, 3, 8);
 
-    int stripe = 0;
-    for (int i=0; i<image.ImageHeight(); ++i) {
-        for (int j=0; j<image.ImageWidth(); ++j) {            
-            if (stripe % 3 == 0) {
-                image.PixelAt(i, j).Blue = 255;
-                image.PixelAt(i, j).Green = 0;
-                image.PixelAt(i, j).Red = 0;             
-            } else if (stripe % 3 == 1) {
-                image.PixelAt(i, j).Blue = 0;
-                image.PixelAt(i, j).Green = 255;
-                image.PixelAt(i, j).Red = 0;
-            } else {
-                image.PixelAt(i, j).Blue = 0;
-                image.PixelAt(i, j).Green = 0;
-                image.PixelAt(i, j).Red = 255;
-            }
+    image.DrawCircle(300, 300, 10, taswira::Colors::White);
 
-            if (i % 50 == 0) {
-                ++stripe;
-            }
-        }
-    }
-
-    image.WriteToDisk("myimage.bmp");
+    image.WriteToDisk("nyalia.bmp");
 
     return 0;
 }
