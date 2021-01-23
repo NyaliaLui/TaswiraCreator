0be5ed5d4b32fbf0538ddc14e432be3ee78db587
Nyalia Lui <luinyalia@gmail.com>
2021-01-23 12:36:11 -0500

    Bring in code from main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "taswira/BitmapImage.h"
+
+int main(void) {
+    taswira::BitmapImage image(14, 40, 512, 512, 3, 8);
+
+    image.DrawCircle(300, 300, 10, taswira::Colors::White);
+
+    image.WriteToDisk("nyalia.bmp");
+
+    return 0;
+}
