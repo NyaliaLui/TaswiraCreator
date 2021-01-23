5004c7dad19f20e2394f06de53934b5acbbe418f
Nyalia Lui <luinyalia@gmail.com>
2021-01-23 02:43:12 -0500

    updated make file

--- make.bat
+++ make.bat
@@ -6,17 +6,16 @@ if %1%==clean (
     del tests.obj
     del taswira.exe
     del tests.exe
-    del *.bmp
 )
 
 if %1%==all (
-    echo "Compiling All... "
+    echo "Compiling All ... "
     cl /EHsc /std:c++17 main.cpp /link /out:taswira.exe
     cl /EHsc /std:c++17 Tests\tests.cpp /link
 )
 
 if %1%==main (
-    echo "Compiling All... "
+    echo "Compiling Main ... "
     cl /EHsc /std:c++17 main.cpp /link /out:taswira.exe
 )
 
