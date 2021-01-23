a6c8d7d552806631b92c001163aaf87c763642fd
Nyalia Lui <luinyalia@gmail.com>
2021-01-23 02:41:55 -0500

    Created circle and square examples

--- Examples/square.cpp
+++ Examples/square.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
-#include "taswira/BitmapImage.h"
+#include "BitmapImage.h"
 
 int main(void) {
-
     taswira::BitmapImage image(14, 40, 512, 512, 3, 8);
 
-    int stripe = 0;
-    for (int i=0; i<image.ImageHeight(); ++i) {
-        for (int j=0; j<image.ImageWidth(); ++j) {            
-            if (stripe % 3 == 0) {
-                image.PixelAt(i, j).Blue = 255;
-                image.PixelAt(i, j).Green = 0;
-                image.PixelAt(i, j).Red = 0;             
-            } else if (stripe % 3 == 1) {
-                image.PixelAt(i, j).Blue = 0;
-                image.PixelAt(i, j).Green = 255;
-                image.PixelAt(i, j).Red = 0;
-            } else {
-                image.PixelAt(i, j).Blue = 0;
-                image.PixelAt(i, j).Green = 0;
-                image.PixelAt(i, j).Red = 255;
-            }
+    image.DrawFilledInSquare(image.ImageBottomMargin(), image.ImageLeftMargin(), 50, 50, taswira::Colors::Blue);
+    image.DrawEmptySquare(image.ImageTopMargin(), image.ImageLeftMargin(), 50, 50, taswira::Colors::Blue);
 
-            if (i % 50 == 0) {
-                ++stripe;
-            }
-        }
+    // draw line along right margin
+    for (int row = image.ImageBottomMargin(); row < image.ImageBottomMargin() + 50; ++row) {
+        image.PixelAt(row, image.ImageRightMargin()).Blue = taswira::Colors::White.Blue;
+        image.PixelAt(row, image.ImageRightMargin()).Green = taswira::Colors::White.Green;
+        image.PixelAt(row, image.ImageRightMargin()).Red = taswira::Colors::White.Red;
     }
 
-    image.WriteToDisk("myimage.bmp");
+    image.WriteToDisk("nyalia.bmp");
 
     return 0;
 }
